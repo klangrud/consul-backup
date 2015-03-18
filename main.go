a0c954d36a25452d5f677c9b535e7b0c473f9f01
Kurt Langrud <kurt.langrud@buckle.com>
2015-03-18 16:09:40 -0500

    added acl backup, without restore

--- main.go
+++ main.go
@@ -6,7 +6,7 @@ import (
     "os"
     "io"
     "strings"
-	"github.com/armon/consul-api"
+	"github.com/hashicorp/consul/api"
     "github.com/docopt/docopt-go"
 )
 
@@ -21,7 +21,7 @@ import (
 //    Session     string
 //}
 
-type ByCreateIndex consulapi.KVPairs
+type ByCreateIndex api.KVPairs
 
 func (a ByCreateIndex) Len() int           { return len(a) }
 func (a ByCreateIndex) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
@@ -31,12 +31,12 @@ func (a ByCreateIndex) Less(i, j int) bool { return a[i].CreateIndex < a[j].Crea
 
 func backup(ipaddress string, token string, outfile string) {
 
-    config := consulapi.DefaultConfig()
+    config := api.DefaultConfig()
     config.Address = ipaddress
-    if token != "" { config.Token = token }
+    config.Token = token
 
 
-	client, _ := consulapi.NewClient(config)
+	client, _ := api.NewClient(config)
 	kv := client.KV()
 
 	pairs, _, err := kv.List("/", nil)
@@ -60,15 +60,47 @@ func backup(ipaddress string, token string, outfile string) {
     }
 }
 
+func backupAcls(ipaddress string, token string, outfile string) {
+
+    config := api.DefaultConfig()
+    config.Address = ipaddress
+    config.Token = token
+
+
+	client, _ := api.NewClient(config)
+	acl := client.ACL()
+
+	tokens, _, err := acl.List(nil)
+    if err != nil {
+            panic(err)
+        }
+    // sort.Sort(ByCreateIndex(tokens))
+
+    outstring := ""
+	for _, element := range tokens {
+        // outstring += fmt.Sprintf("%s:%s:%s:%s\n", element.ID, element.Name, element.Type, element.Rules)
+        outstring += fmt.Sprintf("====\nID: %s\nName: %s\nType: %s\nRules:\n%s\n", element.ID, element.Name, element.Type, element.Rules)
+	}
+
+    file, err := os.Create(outfile)
+    if err != nil {
+        panic(err)
+    }
+
+    if _, err := file.Write([]byte(outstring)[:]); err != nil {
+        panic(err)
+    }
+}
+
 /* File needs to be in the following format:
     KEY1:VALUE1
     KEY2:VALUE2
 */
 func restore(ipaddress string, token string, infile string) {
 
-    config := consulapi.DefaultConfig()
+    config := api.DefaultConfig()
     config.Address = ipaddress
-    if token != "" { config.Token = token }
+    config.Token = token
 
     file, err := os.Open(infile)
     if err != nil {
@@ -79,14 +111,14 @@ func restore(ipaddress string, token string, infile string) {
     _, err = file.Read(data)
     if err != nil && err != io.EOF { panic(err) }
 
-    client, _ := consulapi.NewClient(config)
+    client, _ := api.NewClient(config)
     kv := client.KV()
 
     for _, element := range strings.Split(string(data), "\n") {
         kvp := strings.Split(element, ":")
 
         if len(kvp) > 1 {
-            p := &consulapi.KVPair{Key: kvp[0], Value: []byte(kvp[1])}
+            p := &api.KVPair{Key: kvp[0], Value: []byte(kvp[1])}
             _, err := kv.Put(p, nil)
             if err != nil {
                 panic(err)
@@ -97,32 +129,38 @@ func restore(ipaddress string, token string, infile string) {
 
 func main() {
 
-    usage := `Consul Backup and Restore tool.
+    usage := `Consul KV and ACL Backup with KV Restore tool.
 
 Usage:
-  consul-backup [-i IP:PORT] [-t TOKEN] [--restore] <filename>
+  consul-backup [-i IP:PORT] [-t TOKEN] [--aclbackup] [--aclbackupfile ACLBACKUPFILE] [--restore] <filename>
   consul-backup -h | --help
   consul-backup --version
 
 Options:
-  -h --help     Show this screen.
-  --version     Show version.
-  -i, --address=IP:PORT  The HTTP endpoint of Consul [default: 127.0.0.1:8500].
-  -t, --token=TOKEN  An ACL Token with proper permissions in Consul [default: ].
-  -r, --restore     Activate restore mode`
+  -h --help                          Show this screen.
+  --version                          Show version.
+  -i, --address=IP:PORT              The HTTP endpoint of Consul [default: 127.0.0.1:8500].
+  -t, --token=TOKEN                  An ACL Token with proper permissions in Consul [default: ].
+  -a, --aclbackup                    Backup ACLs, does nothing in restore mode. ACL restore not available at this time.
+  -b, --aclbackupfile=ACLBACKUPFILE  ACL Backup Filename [default: acl.bkp].
+  -r, --restore                      Activate restore mode`
 
-    arguments, _ := docopt.Parse(usage, nil, true, "consul-backup 1.0", false)
-    // fmt.Println(arguments)
-    if arguments["--restore"] == true {
-		fmt.Println("Restore mode:")
-		fmt.Printf("Warning! This will overwrite existing kv. Press [enter] to continue; CTL-C to exit")
-		fmt.Scanln()
-		fmt.Println("Restoring KV from file: ", arguments["<filename>"].(string))
-        restore(arguments["--address"].(string), arguments["--token"].(string), arguments["<filename>"].(string))
-    } else {
-		fmt.Println("Backup mode:")
-		fmt.Println("KV store will be backed up to file: ", arguments["<filename>"].(string))
-        backup(arguments["--address"].(string), arguments["--token"].(string), arguments["<filename>"].(string))
+  arguments, _ := docopt.Parse(usage, nil, true, "consul-backup 1.0", false)
+  fmt.Println(arguments)
+
+  if arguments["--restore"] == true {
+    fmt.Println("Restore mode:")
+    fmt.Printf("Warning! This will overwrite existing kv. Press [enter] to continue; CTL-C to exit")
+    fmt.Scanln()
+    fmt.Println("Restoring KV from file: ", arguments["<filename>"].(string))
+    restore(arguments["--address"].(string), arguments["--token"].(string), arguments["<filename>"].(string))
+  } else {
+    fmt.Println("Backup mode:")
+    fmt.Println("KV store will be backed up to file: ", arguments["<filename>"].(string))
+    backup(arguments["--address"].(string), arguments["--token"].(string), arguments["<filename>"].(string))
+    if arguments["--aclbackup"] == true {
+      fmt.Println("ACL Tokens will be backed up to file: ", arguments["--aclbackupfile"].(string))
+      backupAcls(arguments["--address"].(string), arguments["--token"].(string), arguments["--aclbackupfile"].(string))
     }
-
+  }
 }
